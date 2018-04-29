7108b67f3210e849d800ac79e6a1f9102ca0060f
Dendi Handian <dendihandian@gmail.com>
2018-04-29 16:42:06 +0700

    adding a header and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template lang="html">
   <div class="container">
-    <router-view></router-view>
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view> <!-- Portfolio and Stock Template will goes here -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Header from './components/Header.vue';
   export default {
+      components: {
+        appHeader: Header
+      }
   }
 </script>
 
 <style lang="css">
+  body {
+    padding: 30px;
+  }
 </style>
